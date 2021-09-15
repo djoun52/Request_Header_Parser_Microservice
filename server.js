1306ab03baeb510f8283bca83d1583e1d2fd2582
djoun52 <simon.rioux.trv@gmail.com>
2021-09-15 21:36:13 +0200

    possible fin

--- server.js
+++ server.js
@@ -25,9 +25,17 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami", function (req, res) {
+  var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
+  res.json({
+    ipaddress : ip,
+    language : 'r-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
+    software : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 OPR/78.0.4093.214"
+  });
+});
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT  || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
